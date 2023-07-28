c17493686f2a61fe0f4d9f033f42e4a2b22cac5a
itsroyalrao <my701319@gmail.com>
2023-07-28 09:44:12 +0530

    Saving the user Details on Crud Crud

--- main.js
+++ main.js
@@ -1,203 +1,99 @@
-// AXIOS GLOBALS
-axios.defaults.headers.common['X-AUTH-TOKEN'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
-// GET REQUEST
-function getTodos() {
-  axios
-    .get('https://jsonplaceholder.typicode.com/todos?_limit=5', { timeout: 5000 })
-    .then(res => showOutput(res))
-    .catch(err => console.error(err));
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
+// Add item
+function addItem(e) {
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+  var newItemValue = document.getElementById('item-value').value;
+
+  // Using axios
+  const obj = {
+    newItem,
+    newItemValue
+  }
+
+  const axiosData = axios.post('https://crudcrud.com/api/b28d40da2f0446da932f2c4f2fac8e1f/appointmentData', obj);
+
+  axiosData
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+
+  // Local Storage
+  // localStorage.setItem('one', newItem);
+  // localStorage.setItem('two', newItemValue);
+  // localStorage.clear();
+
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem + ' '));
+  li.appendChild(document.createTextNode(newItemValue));
+
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+
+  // EDIT button
+  var editBtn = document.createElement('button');
+  editBtn.className = 'btn btn-sm float-right';
+  editBtn.appendChild(document.createTextNode('EDIT'));
+  li.appendChild(editBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+
 }
 
-// POST REQUEST
-function addTodo() {
-  axios
-    .post('https://jsonplaceholder.typicode.com/todos', {
-      title: 'New Todo',
-      completed: false
-    })
-    .then(res => showOutput(res))
-    .catch(err => console.error(err));
-}
-
-// PUT/PATCH REQUEST
-function updateTodo() {
-  axios
-    .patch('https://jsonplaceholder.typicode.com/todos/1', {
-      title: 'Updated Todo',
-      completed: true
-    })
-    .then(res => showOutput(res))
-    .catch(err => console.error(err));
-  // axios
-  //   .put('https://jsonplaceholder.typicode.com/todos/1', {
-  //     title: 'Updated Todo',
-  //     completed: true
-  //   })
-  //   .then(res => showOutput(res))
-  //   .catch(err => console.error(err));
-}
-
-// DELETE REQUEST
-function removeTodo() {
-  axios
-    .delete('https://jsonplaceholder.typicode.com/todos/1')
-    .then(res => showOutput(res))
-    .catch(err => console.error(err));
-}
-
-// SIMULTANEOUS DATA
-function getData() {
-  axios.all([
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5'),
-    axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')
-  ])
-    .then(axios.spread((todos, posts) => showOutput(posts)))
-    .catch(err => console.error(err))
-}
-
-// CUSTOM HEADERS
-function customHeaders() {
-  const config = {
-    headers: {
-      'Content-type': 'application/json',
-      Authorization: 'sometoken'
+// Remove item
+function removeItem(e) {
+  if (e.target.classList.contains('delete')) {
+    if (confirm('Are You Sure?')) {
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
     }
   }
-
-  axios
-    .post('https://jsonplaceholder.typicode.com/todos', {
-      title: 'New Todo',
-      completed: false
-    }, config)
-    .then(res => showOutput(res))
-    .catch(err => console.error(err));
 }
 
-// TRANSFORMING REQUESTS & RESPONSES
-function transformResponse() {
-  const options = {
-    method: 'post',
-    url: 'https://jsonplaceholder.typicode.com/todos',
-    data: {
-      title: 'Hello World'
-    },
-    transformResponse: axios.defaults.transformResponse.concat(data => {
-      data.title = data.title.toUpperCase();
-      return data;
-    })
-  }
-
-  axios(options).then(res => showOutput(res))
+// Filter Items
+function filterItems(e) {
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get list
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function (item) {
+    var itemName = item.firstChild.textContent;
+    var itemValue = item.firstChild.nextSibling.textContent;
+    if (itemName.toLowerCase().indexOf(text) != -1) {
+      item.style.display = 'block';
+    }
+    else if (itemValue.toLowerCase().indexOf(text) != -1) {
+      item.style.display = 'block';
+    }
+    else {
+      item.style.display = 'none';
+    }
+  });
 }
-
-// ERROR HANDLING
-function errorHandling() {
-  axios
-    .get('https://jsonplaceholder.typicode.com/todoss', {
-      validateStatus: function (status) {
-        return status < 500;
-      }
-    })
-    .then(res => showOutput(res))
-    .catch(err => {
-      if (err.response) {
-        console.log(err.response.data);
-        console.log(err.response.status);
-        console.log(err.response.headers);
-
-        if (err.response.status === 404) {
-          alert('Error: Page not found');
-        } else if (err.request) {
-          console.error(err.request);
-        } else {
-          console.error(err.message);
-        }
-      }
-    });
-}
-
-// CANCEL TOKEN
-function cancelToken() {
-  const source = axios.CancelToken.source()
-
-  axios
-    .get('https://jsonplaceholder.typicode.com/todos', {
-      cancelToken: source.token
-    })
-    .then(res => showOutput(res))
-    .catch(thrown => {
-      if (axios.isCancel(thrown)) {
-        console.log('Request is cancelled', thrown.message);
-      }
-    })
-
-  if (true) {
-    source.cancel('Request cancelled!')
-  }
-}
-
-// INTERCEPTING REQUESTS & RESPONSES
-
-axios.interceptors.request.use(config => {
-  console.log(`${config.method.toUpperCase()} request sent to ${config.url} at ${new Date().getTime()}`);
-  return config;
-}, error => {
-  return Promise.reject(error);
-})
-
-// AXIOS INSTANCES
-const axiosInstance = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com'
-})
-
-axiosInstance.get('/comments').then(res => showOutput(res));
-
-// Show output in browser
-function showOutput(res) {
-  document.getElementById('res').innerHTML = `
-  <div class="card card-body mb-4">
-    <h5>Status: ${res.status}</h5>
-  </div>
-
-  <div class="card mt-3">
-    <div class="card-header">
-      Headers
-    </div>
-    <div class="card-body">
-      <pre>${JSON.stringify(res.headers, null, 2)}</pre>
-    </div>
-  </div>
-
-  <div class="card mt-3">
-    <div class="card-header">
-      Data
-    </div>
-    <div class="card-body">
-      <pre>${JSON.stringify(res.data, null, 2)}</pre>
-    </div>
-  </div>
-
-  <div class="card mt-3">
-    <div class="card-header">
-      Config
-    </div>
-    <div class="card-body">
-      <pre>${JSON.stringify(res.config, null, 2)}</pre>
-    </div>
-  </div>
-`;
-}
-
-// Event listeners
-document.getElementById('get').addEventListener('click', getTodos);
-document.getElementById('post').addEventListener('click', addTodo);
-document.getElementById('update').addEventListener('click', updateTodo);
-document.getElementById('delete').addEventListener('click', removeTodo);
-document.getElementById('sim').addEventListener('click', getData);
-document.getElementById('headers').addEventListener('click', customHeaders);
-document
-  .getElementById('transform')
-  .addEventListener('click', transformResponse);
-document.getElementById('error').addEventListener('click', errorHandling);
-document.getElementById('cancel').addEventListener('click', cancelToken);
